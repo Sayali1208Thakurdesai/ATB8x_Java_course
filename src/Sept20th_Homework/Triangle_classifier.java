5e82fcf00b8b8d70467a1a37a6d8de9bc3726143
Sayali1208Thakurdesai <sayali.vtdesai@gmail.com>
2024-11-13 10:36:33 +0530

    commit code

--- src/Sept20th_Homework/Triangle_classifier.java
+++ src/Sept20th_Homework/Triangle_classifier.java
@@ -1,4 +1,21 @@
 package Sept20th_Homework;
 
 public class Triangle_classifier {
+    public static void main(String[] args) {
+
+        int side1 = 5;
+        int side2 = 3;
+        int side3 = 2;
+
+        if (side1 == side2 && side2 == side3 && side3 == side1) {
+
+            System.out.println("The triangle is equilateral");
+        } else if (side1 == side2 || side2 == side3 || side3 == side1) {
+            System.out.println("The triangle is isosceles");
+        } else {
+            System.out.println("The triangle is scalene");
+        }
+
+
+    }
 }
